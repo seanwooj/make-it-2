f0cd887d9cda3bb04f58e7347d102380aaf64dd8
seanwooj <seanwoojinkim@gmail.com>
2013-07-13 21:50:51 -0700

    add latitude and longitude to location

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130714032327) do
+ActiveRecord::Schema.define(version: 20130714044955) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,8 @@ ActiveRecord::Schema.define(version: 20130714032327) do
     t.integer  "user_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.float    "latitude"
+    t.float    "longitude"
   end
 
   create_table "users", force: true do |t|
