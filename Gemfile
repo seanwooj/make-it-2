06a0930fe9130c8dd0358d9fedddf7b40a3c31c5
seanwooj <seanwoojinkim@gmail.com>
2013-07-13 19:50:32 -0700

    initial commit. using rpsec, haml, foundation, rails4, pg

---
+++ Gemfile
@@ -0,0 +1,29 @@
+source 'https://rubygems.org'
+
+gem 'rails', '4.0.0'
+gem 'pg'
+
+gem 'sass-rails', '~> 4.0.0'
+gem 'haml-rails'
+gem 'uglifier', '>= 1.3.0'
+gem 'coffee-rails', '~> 4.0.0'
+gem 'jquery-rails'
+gem 'turbolinks'
+gem 'jbuilder', '~> 1.2'
+gem 'compass-rails'
+gem 'zurb-foundation', '~> 4.0.0'
+
+group :doc do
+  # bundle exec rake doc:rails generates the API under doc/api.
+  gem 'sdoc', require: false
+end
+
+gem 'debugger', group: [:development, :test]
+gem "rspec-rails", group: [:test, :development]
+
+group :test do
+  gem 'factory_girl_rails'
+  gem 'capybara'
+  gem 'guard-rspec'
+  gem 'terminal-notifier-guard'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MakeIt2::Application.config.secret_key_base = 'c2cb305b92d26ef6cd5413d36c052cb4069a17a0a771ce591b7dc9e85bcaf02ee3aeaeab3b7143e6e10409ac62ddcb55d3c596845647745fa50830f907ee75ed'
