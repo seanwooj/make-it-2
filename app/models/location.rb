b32db0a9f05fa2a889a1d4b7e29726daf4ac2a13
seanwooj <seanwoojinkim@gmail.com>
2013-07-13 21:48:09 -0700

    added geocoded address method

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,3 +1,11 @@
 class Location < ActiveRecord::Base
   validates :address_1, :city, :zipcode, :state, :country, :user_id, presence: true
+  after_validation :create_geocoded_address
+
+  private
+
+  def create_geocoded_address
+    geocoded_string = "#{self.address_1}, #{self.city}, #{self.state} #{self.zipcode}, #{self.country}"
+    self.address_for_geocoder = geocoded_string
+  end
 end
--- spec/models/location_spec.rb
+++ spec/models/location_spec.rb
@@ -1,8 +1,7 @@
 require 'spec_helper'
 
 describe Location do
-  context 'New location object' do
-    let!(:location) { Location.new(
+  subject(:valid_location) { Location.new(
       address_1: "86 Lessay",
       address_2: "Upstairs Bedroom",
       city: "Newport Coast",
@@ -10,20 +9,37 @@ describe Location do
       zipcode: "92657",
       country: "United States of America",
       user_id: 1
-    ) }
+  ) }
 
+  context 'New location object' do
     it 'is valid when all fields are populated' do
-      location.should be_valid
+      valid_location.should be_valid
     end
 
     it 'is not valid with fields removed' do
-      location.address_1 = nil
-      location.should_not be_valid
+      valid_location.address_1 = nil
+      valid_location.should_not be_valid
     end
 
     it 'is valid without address_2' do
-      location.address_2 = nil
-      location.should be_valid
+      valid_location.address_2 = nil
+      valid_location.should be_valid
+    end
+  end
+
+  context 'geocoder' do
+    describe '#create_geocoded_address' do
+      it 'creates a geocoded address from the fields' do
+        valid_location.send(:create_geocoded_address)
+        valid_location.save!
+        valid_location.address_for_geocoder.should == "86 Lessay, Newport Coast, California 92657, United States of America"
+      end
+
+      it 'creates a geocoded address when the model is saved' do
+        valid_location.address_for_geocoder.should be_nil
+        valid_location.save!
+        valid_location.address_for_geocoder.should == "86 Lessay, Newport Coast, California 92657, United States of America"
+      end
     end
   end
 end
