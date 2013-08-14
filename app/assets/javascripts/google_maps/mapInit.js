0cc66565578e4137cbe5009bd3490888848f3d02
seanwooj <seanwoojinkim@gmail.com>
2013-08-14 09:53:17 -0700

    created mapinit.js

--- app/assets/javascripts/google_maps/mapInit.js
+++ app/assets/javascripts/google_maps/mapInit.js
@@ -6,4 +6,9 @@ function initMap(){
     };
     // Make sure this doesn't interfere with other google map elements
     window.map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+
+    var infowindow = new google.maps.InfoWindow();
+    var marker = new google.maps.Marker({
+        map: map
+    });
 }
